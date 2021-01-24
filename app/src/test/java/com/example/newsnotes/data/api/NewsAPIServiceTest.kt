5e45426528135604ffd7e816e2a5ecc3c057ce8e
Rafi6677 <rafalgiemza6677@gmail.com>
2021-01-24 12:45:33 +0100

    Unit testing NewsAPIService with Mock Web Server

---
+++ app/src/test/java/com/example/newsnotes/data/api/NewsAPIServiceTest.kt
@@ -0,0 +1,81 @@
+package com.example.newsnotes.data.api
+
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okio.buffer
+import okio.source
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class NewsAPIServiceTest {
+
+    private lateinit var service: NewsAPIService
+    private lateinit var server: MockWebServer
+
+    @Before
+    fun setUp() {
+        server = MockWebServer()
+        service = Retrofit.Builder()
+            .baseUrl(server.url(""))
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NewsAPIService::class.java)
+    }
+
+    private fun enqueueMockResponse(fileName: String) {
+        val inputStream = javaClass.classLoader!!.getResourceAsStream(fileName)
+        val source = inputStream.source().buffer()
+        val mockResponse = MockResponse()
+
+        mockResponse.setBody(source.readString(Charsets.UTF_8))
+        server.enqueue(mockResponse)
+    }
+
+    @After
+    fun tearDown() {
+        server.shutdown()
+    }
+
+    @Test
+    fun getTopHeadlines_sentRequest_receivedExpected() {
+        runBlocking {
+            enqueueMockResponse("newsresponse.json")
+            val responseBody = service.getTopHeadlines("pl", 1).body()
+            val request = server.takeRequest()
+
+            assertThat(responseBody).isNotNull()
+            assertThat(request.path).isEqualTo("/v2/top-headlines?country=pl&page=1&apiKey=13e6bd18b4d845569277788377bac221")
+        }
+    }
+
+    @Test
+    fun getTopHeadlines_receivedResponse_correctPageSize() {
+        runBlocking {
+            enqueueMockResponse("newsresponse.json")
+            val responseBody = service.getTopHeadlines("pl", 1).body()
+            val articleList = responseBody!!.articles
+
+            assertThat(articleList.size).isEqualTo(20)
+        }
+    }
+
+    @Test
+    fun getTopHeadlines_receivedResponse_correctContent() {
+        runBlocking {
+            enqueueMockResponse("newsresponse.json")
+            val responseBody = service.getTopHeadlines("pl", 1).body()
+            val articleList = responseBody!!.articles
+            val article = articleList[0]
+
+            assertThat(article.author).isEqualTo("Damian Słomski")
+            assertThat(article.url).isEqualTo("https://www.money.pl/gielda/radoslaw-domagalski-labedzki-nowym-prezesem-rafako-byly-wiceminister-ma-wazna-misje-6600617732111232a.html")
+            assertThat(article.publishedAt).isEqualTo("2021-01-24T09:45:00Z")
+        }
+    }
+
+}
