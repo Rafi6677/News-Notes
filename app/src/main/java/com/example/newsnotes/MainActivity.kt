1561fa22080efa289d7563328850d948cba449ca
Rafi6677 <rafalgiemza6677@gmail.com>
2021-02-08 21:29:14 +0100

    Displaying news headlines in RecyclerView with DiffUtil

--- app/src/main/java/com/example/newsnotes/MainActivity.kt
+++ app/src/main/java/com/example/newsnotes/MainActivity.kt
@@ -2,13 +2,23 @@ package com.example.newsnotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.newsnotes.databinding.ActivityMainBinding
+import com.example.newsnotes.presentation.viewmodel.NewsViewModel
+import com.example.newsnotes.presentation.viewmodel.NewsViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var factory: NewsViewModelFactory
+
+    lateinit var viewModel: NewsViewModel
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +30,9 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavigation.setupWithNavController(
             fragment.findNavController()
         )
+
+        viewModel = ViewModelProvider(this, factory)
+            .get(NewsViewModel::class.java)
     }
 
 }
